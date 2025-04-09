4eaf7adcd76ea6f3639e68932f84d85da2070926
Geoffrey Wells <Gwells625@gmail.com>
2025-04-09 13:16:46 -0400

    Selection statement excercise 2 completed.

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school Subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("No to math.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is interesting.");
+                    break;
+                case "physical education":
+                    Console.WriteLine("Physical Education is good for your health.");
+                    break;
+                case "history":
+                    Console.WriteLine("History is helpful so we do not make the same mistakes.");
+                    break;
+                case "english":
+                    Console.WriteLine("English is my favorite too.");
+                    break;
+                default:
+                    Console.WriteLine("I like that too.");
+                    break;
+                
+            }
+            
         }
     }
 }
